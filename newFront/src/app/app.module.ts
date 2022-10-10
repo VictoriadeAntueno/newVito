36f48e0b779e01bc959972ef28f778ac9841eb1b
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-10 17:34:23 -0300

    Sketchy

--- newFront/src/app/app.module.ts
+++ newFront/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExpLabComponent } from './components/exp-lab/exp-lab.component';
+import { EduComponent } from './components/edu/edu.component';
+import { ProyComponent } from './components/proy/proy.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExpLabComponent,
+    EduComponent,
+    ProyComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
