2b88e26519d33641c1b004de381a3de697311af6
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-10 21:36:54 -0300

    formulario login

--- newFront/src/app/app.module.ts
+++ newFront/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule} from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { FormsModule } from '@angular/forms';
     ProyComponent,
     SkillsComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
         
   ],
   imports: [
