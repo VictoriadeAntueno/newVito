f0fcc9c0733ad882b2b904347d274ac72a5f7faf
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-15 12:15:21 -0300

    pruebas postman

--- ap/src/main/java/com/newback/ap/controller/CPersona.java
+++ ap/src/main/java/com/newback/ap/controller/CPersona.java
@@ -8,6 +8,7 @@ import com.newback.ap.dto.dtoPersona;
 import com.newback.ap.entity.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,19 +34,22 @@ public class CPersona {
     public List<Persona> getPersona() {
         return dtopersona.getPersona();
     }
-
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         dtopersona.savePersona(persona);
         return "La persona fue creada correctamente";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         dtopersona.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
